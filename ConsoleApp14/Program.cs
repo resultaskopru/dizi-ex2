d71074539ad733a81447c98b6a2f8fb9594571c4
Resul Taşköprü <resultaskpru@gmail.com>
2021-01-02 17:03:12 +0300

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] d = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i] = r.Next(100);
+            }
+            int kucuksayi = d[0];
+            int buyuksayi = d[0];
+            for (int i = 1; i < d.Length; i++)
+            {
+                if (kucuksayi > d[i])
+                    kucuksayi = d[i];
+                if (buyuksayi < d[i])
+                    buyuksayi = d[i];
+            }
+            Console.WriteLine("en kücük sayi"+kucuksayi);
+            Console.WriteLine("büyük sayi"+buyuksayi);
+        }
+    }
+}
